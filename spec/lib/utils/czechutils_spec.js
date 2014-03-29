fea32de02ebffedf7ad904f539c973fe280c3493
Tomas Dvorak <todvora@gmail.com>
2014-03-29 07:39:27 +0100

    jasmine tests, travis-ci integration

---
+++ spec/lib/utils/czechutils_spec.js
@@ -0,0 +1,31 @@
+var utils = require("../../../lib/utils/czechUtils");
+
+describe('jasmine-node', function () {
+    it("should translate czech labels to english", function (done) {
+        expect(utils.getKeyTranslation("vyrobce")).toEqual("manufacturer");
+        expect(utils.getKeyTranslation("--unknownKey--")).toEqual("--unknownKey--");
+        expect(utils.getKeyTranslation("vyrobce")).toEqual("manufacturer");
+        done();
+    });
+
+    it("should throw exception on invalid label params", function (done) {
+        var noparam = function () {
+            return utils.getKeyTranslation();
+        };
+        var nullparam = function () {
+            return utils.getKeyTranslation(null);
+            ;
+        };
+
+        expect(noparam).toThrow();
+        expect(nullparam).toThrow();
+        done();
+    });
+
+    it("should standardize czech dates", function (done) {
+        expect(utils.getKeyTranslation(utils.getStandardizedDateStr("1. ledna 2013"))).toEqual("1.1.2013");
+        expect(utils.getKeyTranslation(utils.getStandardizedDateStr("1.   ledna   2013"))).toEqual("1.1.2013");
+        expect(utils.getKeyTranslation(utils.getStandardizedDateStr("31.prosince 2013"))).toEqual("31.12.2013");
+       done();
+    });
+});
